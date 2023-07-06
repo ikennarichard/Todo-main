51de9e23657f4b7ce0757947928c52fdcdace79a
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-06 01:16:34 -1100

    Add task form component

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+import { useContext } from "react";
 
 export default function TaskForm({ todos, addTodoItem }) {
   const [task, setTask] = useState('');
+  const { isDarkMode } = useContext(ThemeContext);
 
   function handleChange(e) {
     // handle todo input
@@ -22,22 +25,32 @@ export default function TaskForm({ todos, addTodoItem }) {
 
     }
   }
+
+  const styles = {
+    backgroundColor: isDarkMode ? 'var(--very-dark-blue)' : 
+    'white',
+    color: isDarkMode ? 'var(--light-grayish-blue)' : 
+    'var(--very-dark-grayish-blue)',
+  }
   
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <button 
-        className="custom-btn hover"
-        type="submit"
-        ></button>
+    <div className="form-container">
+      <form onSubmit={handleSubmit} style={styles}>
       <input 
         type="text" 
         placeholder="Create a new todo"
-        className="input-item"
+        className={`input-item ${isDarkMode ? 
+        'light-gray' : 
+        'dark-blue'}`
+      }
         autoFocus
         value={task}
         onChange={handleChange} 
         />
+        <button 
+        className="custom-btn hover"
+        type="submit"
+        ></button>
       </form>
     </div>
   )
