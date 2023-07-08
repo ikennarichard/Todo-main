1bb25e6bed033dcbb1527df27d702ef9b4b87202
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-07 17:32:30 -1100

    Update filter options

--- src/components/FilterOption.jsx
+++ src/components/FilterOption.jsx
@@ -1,5 +1,5 @@
 import { ThemeContext } from "../context/ThemeContext";
-import { useContext } from "react";
+import { useContext, useState , useEffect} from "react";
 
 export default function FilterOption({ 
   todos, 
@@ -8,6 +8,7 @@ export default function FilterOption({
   count, 
  }) {
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const { isDarkMode, } = useContext(ThemeContext);
 
   function clearComplete() {
@@ -18,30 +19,42 @@ export default function FilterOption({
     setFilter(e.target.textContent.trim());
   }
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const filter_styles = {
+    backgroundColor: windowWidth > 1000 ? 'transparent' : isDarkMode ?  
+      'var( --very-dark-desaturated-blue)' : 'white', 
+    color: isDarkMode ? 'var(--dark-grayish-blue)' : 'var(--dark-grayish-blue-light)'
+  }
+
   return (
   <div className="filter-container">
     <div className="items-left" style={{
-      backgroundColor: isDarkMode ? 'var(--very-dark-blue)' :
+      backgroundColor: isDarkMode ? 'var(--very-dark-desaturated-blue)' :
       'white'
       ,
       color: isDarkMode ? 'var(--dark-grayish-blue-light)'
       : 'var(--dark-grayish-blue)',
+      cursor: 'default',
     }}>
       <span>{count} item(s) left</span>
-      <button className='complete' onClick={clearComplete}>
-        Clear Completed
-      </button>
-    </div>
-      
-    <div className="filters"  style={{
-      backgroundColor: isDarkMode ?  'var(--very-dark-blue)' : 
-      'var(--very-light-gray)',
-      color: isDarkMode ? 'var(--dark-grayish-blue)' : 
-      'var(--dark-grayish-blue-light)',
-    }}>
+
+      <div className="filters"  style={filter_styles}>
       <button onClick={setOption}>All</button>
       <button onClick={setOption}>Active</button>
       <button onClick={setOption}>Completed</button>
+    </div>
+      <button className='complete' onClick={clearComplete}>
+        Clear Completed
+      </button>
     </div>
   </div>
   )
